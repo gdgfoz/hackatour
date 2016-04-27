6cd37accb94f26d212efade7c8aadd55eb388997
Valmir Barbosa <suporte@valmirbarbosa.com.br>
2016-04-27 11:28:07 -0300

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,73 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('gdgfoz.hackatour', [])
+    .controller('MainController', MainController);
+
+  function MainController($scope) {
+
+    //data
+    var vm = this;
+    var uid = null;
+    var cadastroRef = new Firebase("https://hackatour.firebaseio.com/cadastros");
+
+    vm.msg = {
+      text  :null,
+      status:true
+    };
+
+    vm.row = {
+      name  : null,
+      email : null
+    };
+
+    //Methods
+    vm.register = register;
+
+    activate();
+    //-----------------------------------------------
+
+    function activate()
+    {
+        logout();
+        authenticate();
+    }
+
+    function authenticate()
+    {
+        cadastroRef.authAnonymously(function(error, authData) {
+            if (error) {
+              console.log("Login Failed!", error);
+
+            } else {
+              uid = authData.uid;
+              console.log("Login success!", uid);
+
+            }
+        });
+    }
+
+    function logout()
+    {
+       cadastroRef.unauth();
+       uid = null;
+    }
+
+    function register()
+    {
+        if ( ! uid ) {
+           vm.msg =  {text:"Problemas ao registrar", status : false};
+        } else{
+           vm.msg =  {text:"Obrigada por se registrar", status : true};
+        }
+
+        var r = cadastroRef.child(uid).set(vm.row);
+        activate();
+    }
+
+
+  }
+
+
+})();
